85991f5b90372e7ed41bc264f796bbda1aee68aa
Ashla-StarWars <167981174+Ashla-StarWars@users.noreply.github.com>
2024-09-25 16:47:37 +0200

    fixed Game class, all to do things done

--- src/worldcup/Game.java
+++ src/worldcup/Game.java
@@ -10,9 +10,9 @@ import java.util.ArrayList;
  */
 public class Game {
 
-    //TODO to uncomment when class Field is created
-    //Field field;
-	Field field;
+    //DONE: to uncomment when class Field is created
+    Field field;
+
     int size;
     ArrayList<Team> teams;
 
@@ -32,37 +32,87 @@ public class Game {
         Defender defender4 = new Defender();
         Defender defender5 = new Defender();
 
-
-        //TODO create 4 defender players for teamA                
+        //TODO create 4 defender players for teamA     
+        Defender defender1A = new Defender();
+        Defender defender2A = new Defender();
+        Defender defender3A = new Defender();
+        Defender defender4A = new Defender();
+        
         //TODO create midfielder players for teamA
+        Midfielder midfielderA = new Midfielder();
+        
         //TODO 2 forward players for teamA
+        Forward forward1A = new Forward();
+        Forward forward2A = new Forward();
         
         //TODO create coach team A
-        
+        Coach coachA = new Coach();
         
         // create players team B
-        Goalkeeper goalkeeperB = new Goalkeeper();        
-        //TODO create 4 defender players for teamB                
+        Goalkeeper goalkeeperB = new Goalkeeper();
+        
+        //TODO create 4 defender players for teamB   
+        Defender defender1B = new Defender();
+        Defender defender2B = new Defender();
+        Defender defender3B = new Defender();
+        Defender defender4B = new Defender();
+        
         //TODO create midfielder players for teamB
+        Midfielder midfielderB = new Midfielder();
+        
         //TODO 2 forward players for teamB
+        Forward forward1B = new Forward();
+        Forward forward2B = new Forward();
         
         //TODO create coach team B
-        
+        Coach coachB = new Coach();
         
         //TODO create teams
-        //Team teamA = new Team();      
+        Team teamA = new Team();      
+        Team teamB = new Team();   
+        
         //TODO add players to teams
-        //teamA.add(player);
+        teamA.players.add(goalkeeperA);
+        teamA.players.add(defender1A);
+        teamA.players.add(defender2A);
+        teamA.players.add(defender3A);
+        teamA.players.add(defender4A);
+        teamA.players.add(midfielderA);
+        teamA.players.add(forward1A);
+        teamA.players.add(forward2A);
+        
+        teamB.players.add(goalkeeperB);
+        teamB.players.add(defender1B);
+        teamB.players.add(defender2B);
+        teamB.players.add(defender3B);
+        teamB.players.add(defender4B);
+        teamB.players.add(midfielderB);
+        teamB.players.add(forward1B);
+        teamB.players.add(forward2B);
+
         //TODO add teams to match
+        Game game1 = new Game();
+        game1.teams.add(teamA);
+        game1.teams.add(teamB);
         
         // start game
+        game1.start();
+        
         // half time game
+        game1.half_time_game();
+        
         // end game
+        game1.end_game();
     }
 
     public void start() {
         System.out.println("starting");
     }
-
+    public void half_time_game() {
+        System.out.println("half time game");
+    }
+    public void end_game() {
+        System.out.println("end game");
+    }
     
 }
