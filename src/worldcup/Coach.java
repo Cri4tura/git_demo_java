bd280fac61fdc2e9485fa497b9b1f48ef51c44db
Ashla-StarWars <167981174+Ashla-StarWars@users.noreply.github.com>
2024-09-25 16:15:46 +0200

    cambios diversos

--- src/worldcup/Coach.java
+++ src/worldcup/Coach.java
@@ -1,11 +1,9 @@
 package worldcup;
 
 public class Coach extends Person{
-	//Jiahao Changes
+
     private boolean active;
     private String style;
-
-
 
     public void train(){
         System.out.println("train");
